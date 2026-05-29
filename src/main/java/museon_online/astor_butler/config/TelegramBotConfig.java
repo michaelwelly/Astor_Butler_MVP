cd30e7a20457abd30ec8158cde02b9582d0e0675
Michael_Poedinenko <michael.poedinenko.mxr@gmail.com>
2026-05-29 21:19:44 +0500

    Disable Telegram polling for local profile

--- src/main/java/museon_online/astor_butler/config/TelegramBotConfig.java
+++ src/main/java/museon_online/astor_butler/config/TelegramBotConfig.java
@@ -3,6 +3,7 @@ package museon_online.astor_butler.config;
 import lombok.Getter;
 import lombok.Setter;
 import museon_online.astor_butler.telegram.utils.TelegramBot;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +19,7 @@ public class TelegramBotConfig {
     private String token;
 
     @Bean
+    @ConditionalOnProperty(prefix = "telegram.bot", name = "enabled", havingValue = "true", matchIfMissing = true)
     public TelegramBotsApi telegramBotsApi(TelegramBot bot) throws TelegramApiException {
         TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
         api.registerBot(bot);
